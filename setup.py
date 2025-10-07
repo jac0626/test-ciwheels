37d532a74b8f694104bc40da18623969b7145f8e
jac <jacllovey@qq.com>
2025-10-07 15:27:17 +0800

    fix: Pass pybind11 cmake path to build

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+import pybind11
 from cmake_build_extension import BuildExtension, CMakeExtension
 
 setup(
@@ -7,6 +8,8 @@ setup(
             name="my_package_core",
             install_prefix="my_package",
             source_dir="src/my_package",
+            # Pass pybind11's cmake dir to cmake
+            cmake_configure_options=[f"-Dpybind11_DIR={pybind11.get_cmake_dir()}"],
         )
     ],
     cmdclass={"build_ext": BuildExtension},
