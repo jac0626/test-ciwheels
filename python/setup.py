e85385cf9749c5c4175c5e3ec5f82be86fc8c665
jac <jacllovey@qq.com>
2025-10-12 12:06:42 +0800

    fix(build): Use absolute path for C source in setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, Extension
+import os
+
+# get the absolute path to the C source file
+here = os.path.abspath(os.path.dirname(__file__))
+source_file = os.path.join(here, '..', 'src', 'hello.c')
 
 setup(
-    ext_modules=[Extension('pyvsag._hello', sources=['../src/hello.c'])],
+    ext_modules=[Extension('pyvsag._hello', sources=[source_file])],
     zip_safe=False,
 )
